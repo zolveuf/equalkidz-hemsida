761ef2be9660fcbe65de1691606a12f3736cc019
zolveuf <zolveuf@gmail.com>
2024-10-14 17:44:24 +0200

    sista;))

--- script.js
+++ script.js
@@ -8,6 +8,6 @@ let menu = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
 menu.onclick = () => {
-    menu.classList.toggle('bx-menu')
+    menu.classList.toggle('bx-x')
     navbar.classList.toggle('open')
 };
