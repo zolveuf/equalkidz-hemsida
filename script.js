371a664f17212a17ae8295b2826503140297dbf7
zolveuf <zolveuf@gmail.com>
2024-10-15 20:46:58 +0200

    kod grej

--- script.js
+++ script.js
@@ -12,4 +12,42 @@ menu.onclick = () => {
     navbar.classList.toggle('open')
 };
 
+function checkCode() {
+    const code = document.getElementById('access-code').value;
+    const correctCode = "equalkidzuf-ljudböcker-facit";
+    
+    if (code === correctCode) {
+        const blurLayer = document.getElementById('blurLayer');
+        blurLayer.classList.add('active'); // Remove blur with smooth transition
+    } else {
+        alert("Fel kod, vänligen försök igen.");
+    }
+}
+
+function toggleExpand(element) {
+    const audio = element.querySelector('audio');
+    const icon = element.querySelector('.icon');
+    
+    // Toggle the 'expanded' class to show or hide the audio
+    if (element.classList.contains('expanded')) {
+        element.classList.remove('expanded');
+        icon.innerHTML = '&#9660;'; // Down arrow
+    } else {
+        element.classList.add('expanded');
+        icon.innerHTML = '&#9650;'; // Up arrow
+    }
+}
+
+function togglePasswordVisibility() {
+    const passwordInput = document.getElementById('access-code');
+    const toggleButtonIcon = document.getElementById('toggle-password').querySelector('.material-icons');
+    
+    if (passwordInput.type === 'password') {
+        passwordInput.type = 'text';
+        toggleButtonIcon.textContent = 'visibility_off'; // Switch to eye-slash icon
+    } else {
+        passwordInput.type = 'password';
+        toggleButtonIcon.textContent = 'visibility'; // Switch back to eye icon
+    }
+}
 
